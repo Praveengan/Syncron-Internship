cd081471870158d6bd9eb0fe8bad0871a312f9fc
Praveengan <pkdganesha1@gmail.com>
2017-02-15 18:29:38 +0530

    corrected code of 14-02-2017

--- 15-02-2017/arithmetic-operation.js
+++ 15-02-2017/arithmetic-operation.js
@@ -1,7 +1,6 @@
 function ini(){
-	var res;
-	var opd1 = document.getElementById("op1").value;
-	var opd2 = document.getElementById("op2").value;
+	opd1 = document.getElementById("op1").value;
+	opd2 = document.getElementById("op2").value;
 	opd1 = parseInt(opd1);
 	opd2 = parseInt(opd2);
 	return;
@@ -9,25 +8,25 @@ function ini(){
 function add(){
 	ini();
 	res = opd1 + opd2;
-	document.getElementById("res").innerHTML = res ;
+	document.getElementById("result").innerHTML = res ;
 	}
 function sub(){ 
 	ini(); 
 	res = opd1 - opd2;
-	document.getElementById("res").innerHTML = res ;
+	document.getElementById("result").innerHTML = res ;
 	}
 function mul(){
 	ini();
 	res = opd1 * opd2;
-	document.getElementById("res").innerHTML = res ;
+	document.getElementById("result").innerHTML = res ;
 	} 
 function div(){
 	ini();
 	res = opd1 / opd2; 
-	document.getElementById("res").innerHTML = res; 
+	document.getElementById("result").innerHTML = res; 
 	} 
 function mod(){
 	ini();
 	res = opd1 % opd2;
-	document.getElementById("res").innerHTML = res;
+	document.getElementById("result").innerHTML = res;
 	}
---
+++ 15-02-2017/welcome.js
@@ -0,0 +1,4 @@
+function sayHello(){
+    var nam = document.getElementById("name").value;
+    document.getElementById("print").innerHTML = "<h2>Hi "+ nam+",Welcome to Syncron!!!</h2>";
+    }
